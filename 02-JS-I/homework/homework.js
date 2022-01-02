aa5186fa5679bbd68274f7387e0a0dd762cb562b
irvingpal <irvingpal@gmail.com>
2022-01-02 20:28:41 -0300

    actualizado 02/01/20:28

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = " hola, estoy Aprendiendo";
 const nuevoNum = 5;
 
 // Crea una variable booleana:
-const nuevoBool = True;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
